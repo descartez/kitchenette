b3ebe46896cd53eebb882083d3153a7c2c5046be
Ryan Stong <rvanstong@gmail.com>
2017-09-29 07:55:31 -0700

    users now have recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929144456) do
+ActiveRecord::Schema.define(version: 20170929145237) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170929144456) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "published", default: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
