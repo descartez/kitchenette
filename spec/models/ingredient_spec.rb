bb5ef82668a73306d766c04e9c784ba27c527a2a
Ryan Stong <rvanstong@gmail.com>
2017-09-29 08:00:36 -0700

    adds in comments about how to implement tests

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # It should belong to a recipe (dependent: destroy)
+
+  # It should have a title
+
+  # It should have a unit of measurement
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
+  # It should belong to a user
 
+  # It should have a title
+
+  # It should have ingredients
+
+  # It should have directions
 end
