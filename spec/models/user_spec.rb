a91fbf30d3d924244545c9100922b7fe0258ee22
Ryan Stong <rvanstong@gmail.com>
2017-10-01 17:37:59 -0700

    adds in remaining tests for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe User, type: :model do
     user = create(:user)
     expect(user.admin).to be(false)
   end
+
+  it "should be able to be made admin" do
+    user = create(:user)
+    user.make_admin!
+    expect(user.admin).to be(true)
+  end
+
+  it "should be able to be made admin and then remove admin" do
+    user = create(:user)
+    user.make_admin!
+    expect(user.admin).to be(true)
+    user.remove_admin!
+    expect(user.admin).to be(false)
+  end
 end
