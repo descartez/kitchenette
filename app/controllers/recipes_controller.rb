252dc9494d663388adcebf60521dba5e3c5f51aa
Ryan Stong <rvanstong@gmail.com>
2017-10-01 17:33:03 -0700

    cleans up recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -65,10 +65,6 @@ class RecipesController < ApplicationController
     end
   end
 
-  def publish
-
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_recipe
